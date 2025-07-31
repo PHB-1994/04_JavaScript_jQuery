b2dc2a647f75e1aea9a7ee99f2ecd42b529a94e7
PHB-1994 <gudqls0991@naver.com>
2025-07-31 14:58:22 +0900

    함수 재사용하여 동물 출력 실습

--- js/16_강아지고양이사진조회.js
+++ js/16_강아지고양이사진조회.js
@@ -48,6 +48,23 @@ function getDogs() {
   // 여기에 코드 작성
   // picsum.photos API 사용하거나 다른 강아지 API 사용
   // 5장의 강아지 사진을 가져와서 표시하세요
+  $.get("https://api.thedogapi.com/v1/images/search?limit=3").done(function (
+    data
+  ) {
+    const threeDoge = data.slice(0, 5);
+    $("#result2").html(
+      threeDoge.map(
+        (dog) => `
+            <div class="photo-grid">
+            <div class="photo-item"> 
+            <img src="${dog.url}">
+            </div>
+            </div>
+            
+            `
+      )
+    );
+  });
 
   /*
             힌트:
@@ -75,6 +92,24 @@ function getSelectedAnimal() {
   // selected 값이 "cat"이면 고양이 사진 4장
   // selected 값이 "dog"이면 강아지 사진 4장
   // if문을 사용해서 조건에 따라 다른 사진 표시
+
+  $.get(`https://api.the${selected}api.com/v1/images/search?limit=3`).done(
+    function (data) {
+      const count = data.slice(0, 5);
+
+      $("#result3").html(
+        count.map(
+          (animal) =>
+            `
+          <div class="photo-item">
+            <img src="${animal.url}"/>
+            <p>${selected} ID = ${animal.id}</p>
+            </div>
+            `
+        )
+      );
+    }
+  );
 }
 
 // 문제 4: 원하는 개수만큼 고양이 사진 가져오기
@@ -92,7 +127,26 @@ function getCatsWithCount() {
 
   // 여기에 코드 작성
   // count 개수만큼 고양이 사진 가져오기
+
   // Array.from({length: count}, (_, i) => ...) 패턴 사용
+  // 마지막에 ,나 ` 설정되는 것을 "" 빈값으로
+  $.get("https://api.thecatapi.com/v1/images/search?limit=10")
+    .done(function (data) {
+      const cats = data.slice(0, count);
+
+      $("#result4").html(
+        `<div class="photo-grid">
+        ${cats
+          .map(
+            (cat) => `<div class="photo-item">
+            <img src="${cat.url}" />
+          </div>`
+          )
+          .join("")}
+      </div>`
+      );
+    })
+    .fail();
 }
 
 // 문제 5: 랜덤 동물 사진 갤러리
@@ -105,4 +159,19 @@ function getRandomGallery() {
   // 고양이 4장 + 강아지 4장 = 총 8장
   // 두 배열을 합쳐서 하나의 갤러리로 표시
   // concat()이나 spread operator(...) 사용 가능
+
+  // 1. https://api.thecatapi.com/v1/images/search?limit=10
+  // 2. https://api.thedogapi.com/v1/images/search?limit=10
+  animal("cat");
+  animal("dog");
+}
+function animal(동물이름) {
+  $.get(`https://api.the${동물이름}api.com/v1/images/search?limit=10`).done(
+    function (data) {
+      const count = data.slice(0, 5);
+      $("#result5").html(
+        $("#result5").html() + count.map((i) => `<img src="${i.url}">`)
+      );
+    }
+  );
 }
