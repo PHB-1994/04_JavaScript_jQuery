1b813fa49a9a260a1076235b8a59144662bfaeda
PHB-1994 <gudqls0991@naver.com>
2025-08-01 15:31:57 +0900

    페이지네이션(첫페이지, 마지막 페이지)

--- 홈페이지실습/js/gallery.js
+++ 홈페이지실습/js/gallery.js
@@ -56,6 +56,12 @@ function getCats() {
 // 이미지 클릭 시 큰 이미지로 보여주기
 function showFullImg(imageUrl) {
   $("#catResult")
+    // .prepend() 선택한 요소의 맨 앞에 새로운 요소(태그)를 추가
+    /*
+  .prepend() = 맨 앞에 새로운 것을 추가
+  .append()  = 맨 뒤에 새로운 것을 이어서 추가
+  .html()    = 내용 전체 교체
+  */
     .prepend(
       `
         <div id="abc" class="cat-modal" onclick="closeFullImg()">
