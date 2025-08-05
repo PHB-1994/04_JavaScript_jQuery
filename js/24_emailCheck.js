63b7ce8f550b291ddede575befe5c994e515a36b
PHB-1994 <gudqls0991@naver.com>
2025-08-05 10:43:56 +0900

    locatStorage 개념 정리

--- js/24_emailCheck.js
+++ js/24_emailCheck.js
@@ -1,20 +1,56 @@
+// 테스트용 회원 데이터
+const userinfo = [
+  {
+    email: "test@test.com",
+    phone: "010-1111-2222",
+  },
+  {
+    email: "user@gmail.com",
+    phone: "010-3333-4444",
+  },
+  {
+    email: "admin@naver.com",
+    phone: "010-5555-6666",
+  },
+  {
+    email: "hello@daum.net",
+    phone: "010-7777-8888",
+  },
+  {
+    email: "sample@korea.kr",
+    phone: "010-9999-0000",
+  },
+];
+
 $(function () {
-  // 1. 확인 버튼 클릭 시 중복 체크
-  $("#check").click(function () {
-    const email = $("#childEmail").val();
+  $("#childEmail").focus();
+});
 
-    // TODO:
-    // - localStorage에서 users 가져오기
-    // - 중복 확인
-    // - 결과 메시지 표시
-    // - 중복되지 않으면 "사용하기" 버튼 활성화
-    localStorage.getItem(users);
-  });
+// 1. 확인 버튼 클릭 시 중복 체크
+$("#check").click(function () {
+  const email = $("#childEmail").val();
 
-  // 2. 사용하기 버튼 클릭
-  $("#send").click(function () {
-    // TODO:
-    // - 부모창의 이메일 입력란에 값 전달
-    // - 팝업창 닫기
-  });
+  // TODO:
+  // - localStorage에서 users 가져오기
+  // - 중복 확인
+  // - 결과 메시지 표시
+  // - 중복되지 않으면 "사용하기" 버튼 활성화
+  let users = JSON.parse(localStorage.getItem("users") || "[]");
+  if (users.length === 0) {
+    users = userinfo;
+    localStorage.setItem("users", JSON.stringify(userinfo));
+  }
+});
+
+// 2. 사용하기 버튼 클릭
+$("#send").click(function () {
+  const email = $("#childEmail").val().trim();
+
+  opener.document.$("#inputEmail").val(email);
+
+  window.close();
+
+  // TODO:
+  // - 부모창의 이메일 입력란에 값 전달
+  // - 팝업창 닫기
 });
--- js/24_signUp.js
+++ js/24_signUp.js
@@ -1,34 +1,61 @@
 $(function () {
-  // 1. 중복확인 버튼 클릭 시 팝업 열기
-  $("#emailCheck").click(function () {
-    // 팝업 설정
-    const popupWidth = 400;
-    const popupHeight = 300;
-    // TODO: 화면 중앙에 팝업 띄우기
+  showUserList();
+});
+// 1. 중복확인 버튼 클릭 시 팝업 열기
+$("#emailCheck").click(function () {
+  // 팝업 설정
+  const popupWidth = 400;
+  const popupHeight = 400;
+  // TODO: 화면 중앙에 팝업 띄우기
 
-    const width = (window.screen.width - popupWidth) / 2;
-    const height = (window.screen.height - popupHeight) / 2;
+  const width = (window.screen.width - popupWidth) / 2;
+  const height = (window.screen.height - popupHeight) / 2;
 
-    const options = `
+  const options = `
     width=${popupWidth},
     height=${popupHeight},
     left=${width},
     top=${height},
     `;
 
-    window.open("24_emailCheck.html", "_blank", options);
-  });
+  window.open("24_emailCheck.html", "_blank", options);
+});
 
-  // 2. 회원가입 폼 제출
-  $("#signupForm").submit(function (e) {
-    e.preventDefault();
+// 2. 회원가입 폼 제출
+$("#signupForm").submit(function (e) {
+  e.preventDefault();
 
-    // TODO:
-    // - 입력값 가져오기
-    // - localStorage에 저장
-    // - 성공 메시지 표시
-    const users = $("#inputEmail").val();
+  const email = $("#inputEmail").val();
+  if (!email) {
+    alert("이메일 중복확인을 먼저 해주세요.");
+    return;
+  }
 
-    localStorage.setItem(users);
-  });
+  const phone = $("#phone").val();
+  if (!phone) {
+    alert("전화번호를 입력해주세요.");
+    return;
+  }
+
+  newUsers = {
+    email: email,
+    phone: phone,
+  };
+
+  let users = JSON.parse(localStorage.getItem("users") || "[]");
+
+  users.push(newUsers);
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  alert("회원가입이 완료되었습니다.");
+  // TODO:
+  // - 입력값 가져오기
+  // - localStorage에 저장
+  // - 성공 메시지 표시
+
+  $("#inputEmail").html("");
+  $("#phone").html("");
+
+  showUserList();
 });
